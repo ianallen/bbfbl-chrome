f3ec73645474e0452036385a53957501bb6ef303
Ian Allen <ian.allen3@gmail.com>
2017-10-30 15:33:43 -0400

    Remove logging comments

--- src/background.ts
+++ src/background.ts
@@ -1,7 +1,5 @@
 function checkTab(tabId, data, tab: chrome.tabs.Tab) {
-  console.log('check')
   if (tab.url && tab.url.indexOf('fantasysports.yahoo.com/nba/39876') > -1) {
-    console.log(true)
     chrome.pageAction.show(tabId);
   }
 }
