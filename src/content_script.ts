537a8310e8acce7813f64ed300a02f3c71c11ff8
Ian Allen <iallen@stackoverflow.com>
2018-10-16 22:13:23 -0400

    removed unused functionality

--- src/content_script.ts
+++ src/content_script.ts
@@ -14,7 +14,6 @@ $(function() {
     }
 
     function render() {
-        console.log("hello ???")
         const rendered = $(salariedSelector).length >  0;
         var val = $(salariedSelector)
         if (rendered) return;
@@ -41,23 +40,6 @@ $(function() {
         $('td .Ov-h ').css('width', width)
     }
 
-    $('.player-status').on('click', function() {
-        
-        let href = $(this).closest('.Grid-bind-end').find('.ysf-player-name a').attr('href');
-        console.log(href)
-        if (!href) return;
-        const playerData = _.find(salaries, { yahoo_id: getId(href) });
-        const value = playerData ? playerData.salary18_19 : 0;
-        const container = $(`#playernote-LDRB-${playerData.yahoo_id}`);
-        console.log(container, container.length)
-        // while (container.length < 1) {
-        //     console.log('yeeee', playerData.yahoo_id)
-        //     //const elems = $.map(salaries, salary => $('<li>' + renderSalary(salary) + '</li>'));
-        //     const elem = $('<li>' + renderSalary(playerData.salary19_20) + '</li>')
-        //     container.append(elem)
-        // }
-    })
-
     function sum(values: number[]) {
         return values.reduce((total, salary) => { return total + salary}, 0);
     }
@@ -79,8 +61,6 @@ $(function() {
         const container = $(`#playernote-LDRB-${playerid}`);
         console.log(container)
         if (container.length == 1) {
-            console.log('yeeee', playerid)
-            //const elems = $.map(salaries, salary => $('<li>' + renderSalary(salary) + '</li>'));
             const elem = $('<li>' + renderSalary(salary) + '</li>')
             container.append(elem)
         }
